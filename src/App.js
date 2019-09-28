b7d028ac0a13554315601f8cd8249e4cf30c4a7d
Mazino Ukah <ewomaukah@yahoo.com>
2019-09-28 18:37:49 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. kkl mm oo jk pp 000
         </p>
         <a
           className="App-link"
